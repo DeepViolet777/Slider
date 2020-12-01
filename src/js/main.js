70d20b05e36e461ed82515919d4ba470c3505cbf
Anna Malorodova <angervenus6@gmail.com>
2020-12-01 20:23:26 +0300

    Filtration finished

--- src/js/main.js
+++ src/js/main.js
@@ -6,50 +6,139 @@ $(document).ready(() => {
   // eslint-disable-next-line no-console
   console.log(`document ready`)
 
+  let swiper;
+  
+
+    const initSlider = () => {
+      swiper = new Swiper('.swiper-container', {
+        slidesPerView: 6,
+        slidesPerColumn: 2,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+    };
+
+  const initialSlides = document.querySelectorAll('.slider__slide');
+ // console.log(initialSlides);
+  
+  
+  const selectFilter = filterVal => {
+    //console.log(filterVal); 
+    const isAllFilter = filterVal === 'all';
+    document.querySelectorAll('.nav__item').forEach(item => {
+      item.classList.toggle('active', item.dataset.filter === filterVal);
+    });
+
+      swiper.removeAllSlides();
+      swiper.prependSlide(Array.prototype.slice.call(initialSlides).filter(slide => isAllFilter || slide.dataset.country === filterVal));
+      swiper.destroy();
+      initSlider();
+    }
+
+  initSlider();
+  
+  document.querySelectorAll('.nav__item').forEach(item => {
+  item.onclick = e => {
+  selectFilter(e.target.dataset.filter);
+  }
+  });
+
+
+
+}); 
+
+
+
+
+
+
+
+
+
+
+
+/*
   const Slider = new Swiper('.slider', {
     slidesPerView: 6,
     spaceBetween: 30,
     slidesPerColumn: 2,
 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+   
   });
 
 
 
 
-  const nav = document.querySelector('.nav__list')
-  const filterCountry = document.querySelectorAll('.swiper-slide');
+  
 
-  nav.addEventListener('click', event => {
+  /*function filterSelected(){
 
-    /*if (event.target.tagName !== 'LI') return false;*/
+    Slider.destroy();
+
+    
+};*/
+  
+  
+    /*const nav = $('.nav__list');
+    const filterCountry = $('.slider__slide');
+
+
+   nav.on('click', function(event){
+
+  let filterClass = event.target.dataset['country'];
+  
+  filterCountry.remove();
+
+  filterCountry.each(function($elem){
+    if($($elem).hasClass(filterClass) && filterClass !== 'all') { 
+    
+        $(".slider__wrapper").append($($elem)[0].outerHTML);
+       }
+
+    });
+
+   });
+
+});*/
+  /*
+   nav.addEventListener('click', event => {
 
     let filterClass = event.target.dataset['country'];
     console.log(filterClass);
 
     filterCountry.forEach(elem => {
-        elem.style.marginRight = "30px";
-        setTimeout(function () {
-            elem.classList.remove('visuallyhidden');
-          }, 200);
-    
-      elem.classList.remove('hidden');
-
-     
-      
-      if (!elem.classList.contains(filterClass) && filterClass !== 'all') {
-        setTimeout(function () {
-            elem.classList.add('visuallyhidden');
-          }, 200);
-        elem.classList.add('hidden');
-        
-        
        
+     /* setTimeout(function () {
+        elem.classList.remove('visuallyhidden');
+      }, 30);
+      
+      elem.classList.remove('hidden');
+    
+      if (!elem.classList.contains(filterClass) && filterClass !== 'all') {
+      
+        elem.classList.add('hidden');
 
-        Slider.destroy();
+       /* elem.style.marginRight = "30px";
+       
+     
+      } 
+     
+    });
+    
+  
+
+  });*/
+
+
+
+
+
+
+
+        
       /*
         document.querySelector("all").addEventListener('click', event =>{
             
@@ -94,12 +183,6 @@ document.querySelector('nav').addEventListener('click', event => {
        if (!elem.classList.contains(filterClass) && filterClass!== 'all') {
           elem.classList.add('hide');
        }
-    });
-});*/
-      }
+    });*/
 
-    });
 
-  });
-
-});
